dda940d12a1ac5e3c5724a435251c7618705e089
Neto test <veronikakaravanova60@gmail.com>
2025-03-23 15:31:54 +0300

    change README

---
+++ src/main/java/ru/netology/javaga/Post.java
@@ -0,0 +1,17 @@
+package ru.netology.javaga;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    boolean subscription;
+    public String day;
+    public String month;
+    public String year;
+
+
+}
+
